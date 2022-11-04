3020f1a21a6eb911e2a97ae6d8c19f908037f195
vladdd955 <74567141+vladdd955@users.noreply.github.com>
2022-11-04 23:00:26 +0200

    Add files via upload

---
+++ Slote-machine/index.php
@@ -0,0 +1,238 @@
+<?php
+
+
+
+$board = [
+   [" "," "," "," "],
+   [" "," "," "," "],
+   [" "," "," "," "],
+];
+
+
+
+function display(array $board)
+{
+    echo " {$board[0][0]}  | {$board[0][1]}  |  {$board[0][2]}  | {$board[0][3]} \n";
+    echo " ---+----+-----+-----\n";
+    echo " {$board[1][0]}  | {$board[1][1]}  |  {$board[1][2]}  | {$board[1][3]} \n";
+    echo " ---+----+-----+-----\n";
+    echo " {$board[2][0]}  | {$board[2][1]}  |  {$board[2][2]}  | {$board[2][3]}  \n";
+
+}
+
+display($board);
+echo "Try to win :) \n";
+
+
+$slotSymbols = [':)', 'X', 'O', '#', '%', '$'];
+
+
+
+
+$playerMoney  = 80;
+$payedForSpin = 3 * 4;
+$columnWin = 15;
+$rowsWin = 20;
+$epicWin = 100;
+
+
+$userSelection =  readline("Press 1 if you want try to win something -> ");
+
+
+
+//line nr. 1
+
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[0][0] = $chose;
+           display($board) . "\n";
+           echo "Fortune's choice. \n";
+           echo "\n";
+       }
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[1][0] = $chose;
+           display($board) . "\n";
+           echo "Fortune's choice. \n";
+           echo "\n";
+       }
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[2][0] = $chose;
+           display($board);
+           echo "\n";
+           echo "\n";
+
+       }
+//line nr 2
+       $userSelection = readline("Press 1 if you want try to win something -> ");
+
+
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[0][1] = $chose;
+           display($board) . "\n";
+           echo "Fortune's choice. \n";
+           echo "\n";
+       }
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[1][1] = $chose;
+           display($board) . "\n";
+           echo "Fortune's choice. \n";
+           echo "\n";
+       }
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[2][1] = $chose;
+           display($board);
+           echo "\n";
+           echo "\n";
+       }
+//line nr 3
+       $userSelection = readline("Press 1 if you want try to win something -> ");
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[0][2] = $chose;
+           display($board) . "\n";
+           echo "Fortune's choice. \n";
+           echo "\n";
+       }
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[1][2] = $chose;
+           display($board) . "\n";
+           echo "Fortune's choice. \n";
+           echo "\n";
+       }
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[2][2] = $chose;
+           display($board);
+           echo "\n";
+           echo "\n";
+       }
+//line nr. 4
+       $userSelection = readline("Press 1 if you want try to win something -> ");
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[0][3] = $chose;
+           display($board) . "\n";
+           echo "Fortune's choice. \n";
+           echo "\n";
+       }
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[1][3] = $chose;
+           display($board) . "\n";
+           echo "Fortune's choice. \n";
+           echo "\n";
+       }
+       if ($userSelection == 1) {
+           $symbol = array_rand($slotSymbols);
+           $chose = $slotSymbols[$symbol];
+
+           $board[2][3] = $chose;
+           display($board);
+           echo "Your money after spin -> " . $mm = $playerMoney - $payedForSpin . "\n";
+
+
+}
+
+
+        //Column combination.
+
+function columns($board, $playerMoney, $columnWin)
+{
+
+    if ($board[0][0] === $board[1][0] && $board[0][0] === $board[2][0] ) {
+        $resultColumn  = $playerMoney + $columnWin;
+        echo  "You EARN in column line -> " . $columnWin . " In your wallet have -> ". $resultColumn . "\n";
+    }
+    if ($board[0][1] === $board[1][1] && $board[0][1] === $board[2][1] ) {
+        $resultColumn = $playerMoney + $columnWin;
+        echo  "You EARN in column line -> " . $columnWin . " In your wallet have -> ". $resultColumn . "\n";
+    }
+    if ($board[0][2] === $board[1][2] && $board[0][2] === $board[2][2] ) {
+        $resultColumn = $playerMoney + $columnWin;
+        echo  "You EARN in column line -> " . $columnWin . " In your wallet have -> ". $resultColumn . "\n";
+    }
+    if ($board[0][3] === $board[1][3] && $board[0][3] === $board[2][3] ) {
+        $resultColumn = $playerMoney + $columnWin;
+        echo  "You EARN in column line -> " . $columnWin . " In your wallet have -> ". $resultColumn . "\n";
+    }
+
+}
+columns($board, $playerMoney, $columnWin);
+
+        // rows combination.
+
+function rows($board, $playerMoney, $rowsWin)
+{
+    if ($board[0][0] === $board[0][1] && $board[0][0] === $board[0][2] && $board[0][0] === $board[0][3] ) {
+        $resultRows  = $playerMoney + $rowsWin;
+        echo  "You EARN in row line -> " . $rowsWin . " In your wallet have -> ". $resultRows . "\n";
+    }
+    if ($board[1][0] === $board[1][1] && $board[1][0] === $board[1][2] && $board[1][0] === $board[1][3] ) {
+        $resultRows = $playerMoney + $rowsWin;
+        echo  "You EARN in row line -> " . $rowsWin . " In your wallet have -> ". $resultRows . "\n";
+    }
+    if ($board[2][0] === $board[2][1] && $board[2][0] === $board[2][2] && $board[2][0] === $board[2][3] ) {
+        $resultRows = $playerMoney + $rowsWin;
+        echo  "You EARN in row line -> " . $rowsWin . " In your wallet have -> ". $resultRows . "\n";
+    }
+
+
+}
+rows($board, $playerMoney, $rowsWin);
+
+        //epic combination.
+
+function epic($board, $playerMoney, $epicWin)
+{
+    if($board[0][0] === $board[1][0] && $board[0][0] === $board[2][1] && $board[0][0] === $board[2][2]) {
+        $resultEpic = $playerMoney + $epicWin;
+        echo  "You EARN in epic line -> " . $epicWin . " In your wallet have -> ". $resultEpic . "\n";
+    }
+    if($board[0][3] === $board[1][3] && $board[0][3] === $board[1][2] && $board[0][3] === $board[1][1]) {
+        $resultEpic = $playerMoney + $epicWin;
+        echo  "You EARN in epic line -> " . $epicWin . " In your wallet have -> ". $resultEpic . "\n";
+    }
+    if($board[2][0] === $board[2][1] && $board[2][0] === $board[1][2] && $board[2][0] === $board[0][3]) {
+        $resultEpic = $playerMoney + $epicWin;
+        echo  "You EARN in epic line -> " . $epicWin . " In your wallet have -> ". $resultEpic . "\n";
+    }
+    if($board[2][0] === $board[1][0] && $board[2][0] === $board[0][1] && $board[2][0] === $board[1][2] && $board[2][0] === $board[2][2]) {
+        $resultEpic = $playerMoney + $epicWin;
+        echo  "You EARN in epic line -> " . $epicWin . " In your wallet have -> ". $resultEpic . "\n";
+    }
+
+}
+epic($board, $playerMoney, $epicWin);
+
+
+
+
+
